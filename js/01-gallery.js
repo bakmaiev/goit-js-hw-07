f29f0e6ec31271a7d4a078aeb241adb38d64d06d
bakmaiev <thisyoungrhyme@gmail.com>
2023-03-04 19:14:46 +0200

    task01 without escape function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createItemGalleryMarkup(galleryItems));
+
+function createItemGalleryMarkup() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+    }).join('');
+};
+
+const handleImgClick = (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const currentImg = e.target.dataset.source;
+    createImageModal(currentImg);
+};
+
+galleryEl.addEventListener('click', handleImgClick);
+
+function createImageModal(image) {
+    const instance = basicLightbox.create(`
+    <img src="${image}" width="1200">
+`, {
+        onShow: (instance) => {
+        instance.element()
+    }
+})
+instance.show()
+}
