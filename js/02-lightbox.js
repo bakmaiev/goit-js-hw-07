e69aa5ffb785111dc42b08a94bcef476d73e2220
bakmaiev <thisyoungrhyme@gmail.com>
2023-03-04 19:14:59 +0200

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createItemGalleryMarkup(galleryItems));
 
-console.log(galleryItems);
+function createItemGalleryMarkup() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}" 
+                alt="${description}" />
+        </a>`;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
